0f1ef6467812289856178b0aa4c8cbe8203729c4
Shantanu Kalekar <shantanukalekar72@gmail.com>
2022-01-19 13:37:25 +0530

    Add files via upload

---
+++ skbotservice-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\skbotservice-bot-9dfa-Web-Deploy.pubxml -p:Password=hfr1SN1DvxRD4eFvdSGHoeCMWD71jqYYJmQTQfbeQXRBPy2mtspnj4c3GZyx
+
